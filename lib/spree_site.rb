06c95689bfa0d89a4059f8396f899c8c5c867963
Eric Soliz <e.soliz24@yahoo.com>
2011-05-18 14:11:04 -0700

    auto_capture

--- lib/spree_site.rb
+++ lib/spree_site.rb
@@ -2,6 +2,9 @@ module SpreeSite
   class Engine < Rails::Engine
     def self.activate
       # Add your custom site logic here
+      if Spree::Config.instance
+          Spree::Config.set(:auto_capture => true)
+      end
     end
     config.to_prepare &method(:activate).to_proc
   end
