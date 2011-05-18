3b98f27dc8c7fef4366f6aab3e4a5af7b66d5ea0
Eric Soliz <e.soliz24@yahoo.com>
2011-05-18 08:47:31 -0700

    Spree PayPalExpress Base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PPal::Application.config.secret_token = '53173a4b2253cb0e015cc977573ca659a294d99590e597a5883666e45e05eda84a3dbad06fa15c3b1c09eb9ed8bd1e01763cfd987440ecd54955ab097280970d'
